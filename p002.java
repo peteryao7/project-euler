d660c5553636db3df71e5c54679dee9a66d87db8
peteryao7@gmail.com <peteryao7@gmail.com>
2019-12-05 17:05:48 -0800

    Add Q2 Java code

---
+++ p002.java
@@ -0,0 +1,26 @@
+class p002 {
+    /*
+     * By considering the terms in the Fibonacci sequence whose values 
+     * do not exceed four million, find the sum of the even-valued terms.
+     */
+    public static int sumEvenFibos(int n) {
+        int sum = 0;
+        int x = 1; // second 1 in the Fibonacci sequence
+        int y = 2;
+        int z;
+
+        while (x <= 4000000) {
+            if (x % 2 == 0)
+                sum += x;
+            z = x + y;
+            x = y;
+            y = z;
+        }
+
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sumEvenFibos(4000000));
+    }
+}
