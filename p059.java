9ef5a82a9f014364c5f7b169631f7486fd4e566a
peteryao7@gmail.com <peteryao7@gmail.com>
2020-01-25 23:53:45 -0800

    Delete part of Q59 question

--- p059.java
+++ p059.java
@@ -22,10 +22,7 @@ class p059 {
      * message. The balance for this method is using a sufficiently long password
      * key for security, but short enough to be memorable.
      * 
-     * Your task has been made easy, as the encryption key consists of three lower
-     * case characters. Using p059_cipher.txt (right click and 'Save Link/Target
-     * As...'), a file containing the encrypted ASCII codes, and the knowledge that
-     * the plain text must contain common English words, decrypt the message and
+     * The plain text must contain common English words. Decrypt the message and
      * find the sum of the ASCII values in the original text.
      * 
      * -----
